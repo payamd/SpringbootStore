02a6b89c42ec40e42152667541d044aad4c44399
payamd <payamd@users.noreply.github.com>
2025-03-31 00:43:06 -0400

    init

---
+++ src/test/java/com/payamdavoudi/store/StoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payamdavoudi.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
